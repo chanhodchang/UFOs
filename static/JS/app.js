799a1ec4ba6a2d6e970bd456eea22c09b30c2ad7
Daniel Chang <danielchang@Daniels-MacBook-Pro.local>
2020-02-09 15:50:46 -0800

    edited index.html and app.js files and added style.css and nasa.jpg

--- static/JS/app.js
+++ static/JS/app.js
@@ -11,7 +11,7 @@ function buildTable(data) {
     // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
         // Append a row to the table body
-        let row = body.append('tr');
+        let row = tbody.append('tr');
         // Loop through each field in the dataRow and add
         // each value as a table cell (td)
         Object.values(dataRow).forEach((val) => {
@@ -32,14 +32,14 @@ function handleClick() {
         // Apply 'filter' to the tbale data to only keep the 
         // rows where the 'datetime' value matches the filter value
         filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
     // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
     buildTable(filteredData);
 }
 // Attach an event to listen for the form button
-d3.select('#filter-btn').on('click', handleClick);
+d3.selectAll('#filter-btn').on('click', handleClick);
 // Build the table when the pafe loads
 buildTable(tableData);
 
