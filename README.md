692cf06a5e0ddab6c566ee605f20da4f7a849838
Daniel Chang <danielchang@daniels-mbp.lan>
2020-02-10 02:17:41 -0800

    added the challenge portion into app.js and index.html files and edited README.md

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # UFOs
 Using data stored in JavaScript to build a table with filters and placing it into an HTML file.
+
+## Project Overview 
+Used JavaScript to store and hold data about UFO sightings. JavaScript is also used to build tables and fill the table with the data from a JavaScript file with data. The data is displayed using HTML.
+
+The challege project was to create mulitple filters to filter the table in the HTML file. The additional filters created were City, State, Country, and Shape.
+
+## Resources
+- Data Sources: data.js
+- Software: JavaScript 1.8.5, HTML 5, CSS 3
+
+## Summary
+JavaScript was used to create tables and filters for UFO sightings data that was also stored in a JavaScript file. The table was filled with the data and displayed using HTML.
+
+1. First coded a function to create a table using JavaScript
+2. Created a code that displayed the filter box and that would filter out the date in the table.
+3. Coded an HTML template to create a display for the new data.
+4. Created a header to show the title and a subtitle and paragraph description below.
+5. Placed the filter box and table below that.
+6. Added in links to the HTML file to grab the data from data.js
+7. Opened the HTML file and tested to see if the filter worked.
+
+The challenge project was to create multiple filters for City, State, Country, and Shape. The code was written with if statements to stack the filters.
+
+1. First created the function with the updated filters using and if statement to add the called filter onto a filtered list or clear it from the filters object.
+2. Created a filtered table function that would loop through the list of filters and grab the data from the table that would match the filters.
+3. Attached an event to listen for changes within the filtered list.
+
+## Analysis
+JavaScript is a useful tool to create filtered tables that can be displayed in an HTML file. JavaScript has also a way to hold a list of data formatting it similar to a JSON file. When looking at the data, most of the UFO sightings happened during the month of January. The data shows that most to all of the UFO sightings have been in the US. 
--- static/JS/app.js
+++ static/JS/app.js
@@ -1,45 +1,70 @@
-// import the data from data.js
-const tableData=data;
+// from data.js
+const tableData = data;
 
-// Reference the HTML table using d3
-var tbody=d3.select('tbody');
+// get table references
+var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    // First, clear out any existing data
-    tbody.html("");
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
-    data.forEach((dataRow) => {
-        // Append a row to the table body
-        let row = tbody.append('tr');
-        // Loop through each field in the dataRow and add
-        // each value as a table cell (td)
-        Object.values(dataRow).forEach((val) => {
-            let cell=row.append('td');
-            cell.text(val);
-            }
-        );
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
     });
-};
-
-function handleClick() {
-    // Grab the datetime value from the filter
-    let date = d3.select('#datetime').property('value');
-    let filteredData = tableData;
-    // Check the to seet if a date was entered and filter the 
-    // data using that date.
-    if (date) {
-        // Apply 'filter' to the tbale data to only keep the 
-        // rows where the 'datetime' value matches the filter value
-        filteredData = filteredData.filter(row => row.datetime === date);
-    }
-    // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
-    buildTable(filteredData);
+  });
 }
-// Attach an event to listen for the form button
-d3.selectAll('#filter-btn').on('click', handleClick);
-// Build the table when the pafe loads
-buildTable(tableData);
 
+// Keep track of all filters
+var filters = {};
+
+// This function will replace your handleClick function
+function updateFilters() {
+
+  // Save the element, value, and id of the filter that was changed
+  let changedElement = d3.select(this);
+  let changedValue = changedElement.property("value");
+  let changedId = changedElement.attr("id");
+
+  // If a filter value was entered then add that filterId and value
+  if (changedValue) {
+      filters[changedId] = changedValue;
+  }
+  // to the filters list. Otherwise, clear that filter from the filters object
+  else{
+      delete filters[changedId];
+  };
+  // Call function to apply all filters and rebuild the table
+  filterTable();
+}
+
+function filterTable() {
+  // Set the filteredData to the tableData
+  let filteredTable = tableData;
+  
+  // Loop through all of the filters and keep any data that
+  // matches the filter values
+  Object.entries(filters).forEach(([key, value]) => {
+      filteredTable = filteredTable.filter(row => row[key] === value);
+  });
+
+  
+  // Finally, rebuild the table using the filtered Data
+  buildTable(filteredTable);
+}
+
+// Attach an event to listen for changes to each filter
+// Hint: You'll need to select the event and what it is listening for within each set of parenthesis
+d3.selectAll("input").on("change", updateFilters);
+d3.selectAll("#filter-btn").on("click", updateFilters);
+
+// Build the table when the page loads
+buildTable(tableData)
